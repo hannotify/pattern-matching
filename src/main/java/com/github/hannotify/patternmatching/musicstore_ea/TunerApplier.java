39a22def9df9794b322da2a788b1db5d7f484fbe
Peter Wessels <peter.wessels@infosupport.com>
2022-11-01 21:17:57 +0100

    Add comment to suggest implementation

--- src/main/java/com/github/hannotify/patternmatching/musicstore_ea/TunerApplier.java
+++ src/main/java/com/github/hannotify/patternmatching/musicstore_ea/TunerApplier.java
@@ -4,6 +4,7 @@ public class TunerApplier {
     static String apply(Tuner effect, Guitar guitar) {
         return switch(effect) {
             case Tuner tu when guitar.inTune() -> "No tuner active, because Guitar is in tune!";
+//            "Tuner active with pitch 600 on Note A"
             default -> throw new IllegalStateException("Unexpected value: " + effect);
         };
     }
