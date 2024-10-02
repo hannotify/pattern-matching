801fa1c783220f102ba197696bc3519a9a47db5e
Hanno Embregts <hanno.embregts@infosupport.com>
2024-10-02 13:57:08 +0200

    Apply deconstruction patterns

--- src/main/java/com/github/hannotify/patternmatching/musicstore_ea/TunerApplier.java
+++ src/main/java/com/github/hannotify/patternmatching/musicstore_ea/TunerApplier.java
@@ -3,9 +3,8 @@ package com.github.hannotify.patternmatching.musicstore_ea;
 public class TunerApplier {
     static String apply(Tuner effect, Guitar guitar) {
         return switch(effect) {
-            case Tuner tu when guitar.inTune() -> "No tuner active, because Guitar is in tune!";
-//            "Tuner active with pitch 600 on Note A"
-            default -> throw new IllegalStateException("Unexpected value: " + effect);
+            case Tuner _ when guitar.inTune() -> "No tuner active, because Guitar is in tune!";
+            case Tuner(int pitch, Note(String note)) -> "Tuner active with pitch %d on Note %s".formatted(pitch, note);
         };
     }
 }
