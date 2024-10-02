0ba67a5810690a080203360aa153c68c4fd8eb0f
Hanno Embregts <hanno.embregts@infosupport.com>
2024-10-02 13:39:37 +0200

    Apply guarded pattern

--- src/main/java/com/github/hannotify/patternmatching/musicstore/EffectApplier.java
+++ src/main/java/com/github/hannotify/patternmatching/musicstore/EffectApplier.java
@@ -12,7 +12,8 @@ public class EffectApplier {
             case Reverb re     -> String.format("Reverb active of type %s and roomSize %d.", re.getName(), re.getRoomSize());
             case Overdrive ov  -> String.format("Overdrive active with gain %d.", ov.getGain());
             case Tremolo tr    -> String.format("Tremolo active with depth %d and rate %d.", tr.getDepth(), tr.getRate());
-            case Tuner tu      -> String.format("Tuner active with pitch %d. Muting all signal!", tu.getPitchInHz());
+            case Tuner tu when !guitar.isInTune() -> String.format("Tuner active with pitch %d. Muting all signal!", tu.getPitchInHz());
+            case Tuner _ -> "No need to tune, this guitar is pitch-perfect.";
             case EffectLoop el -> el.getEffects().stream().map(e -> apply(e, guitar)).collect(Collectors.joining(System.lineSeparator()));
             default            -> "Unknown effect active.";
         };
